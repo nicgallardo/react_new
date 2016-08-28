991bbfcf0c3db221850851a64fc44403d1814b8a
Nicholas Gallardo <nic.gallardo@gmail.com>
2016-08-27 23:27:01 -0600

    update

--- simple/index.js
+++ simple/index.js
@@ -1,4 +1,9 @@
-var value = "Hello!";
+var value = function(){
+  var test = [1, 2, 3, 4, 5, 6, 7, 8]
+  return test.map(function(val){
+    return <div><b>{val}</b></div>;
+  })
+}
 var something = "React";
 
 var Comp1 = React.createClass({
@@ -15,7 +20,7 @@ var Comp2 = React.createClass({
   render: function(){
     return (
       <div className="textDiv">
-        Well {value}
+        {value()}
       </div>
     )
   }
